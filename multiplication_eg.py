450bbdf4ab3fd1bb6964251647780996901f7a28
manjushaapsunde <manjuapsunde04@gmail.com>
2022-09-02 19:01:54 +0530

    multiplication example

---
+++ multiplication_eg.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+def mul(num1,num2):
+    return num1 * num2
+
+res = mul(num1,num2)
+print(f"Multiplication4 of {num1} and {num2} is {res}")
